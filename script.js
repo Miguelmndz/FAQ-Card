86564e7d0eb307163d3a9fe332d5c239fd7ef282
Miguel Mendez <michaelmndz1141@gmail.com>
2021-03-07 11:55:41 -0700

    Completed mobile view

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
 const accordion = document.getElementsByClassName('card-accordion-item')
 const accordionContent = document.getElementsByClassName('card-accordion-content')
+const accordionHeader = document.getElementsByClassName('card-accordion-header')
+const accordionArrow = document.getElementsByClassName('arrow')
 
 for (let i = 0; i < accordion.length; i++) {
   let currAcc = accordion[i]
   let currAccContent = accordionContent[i]
+  let currAccHeader = accordionHeader[i]
+  let curraccordionArrow = accordionArrow[i]
+
   currAcc.addEventListener("click", () => {
-    // currAcc.classList.toggle('active')
-    
-    
-    if (currAccContent.style.display === 'none') {
-      currAccContent.style.display = 'block'
+    if (currAccContent.style.maxHeight == 0) {
+      currAccHeader.style.fontWeight = '700'
+      currAccContent.style.maxHeight = currAccContent.scrollHeight + 'px'
+      currAccContent.style.marginBottom = '15px'
+      curraccordionArrow.style.transform = 'rotate(180deg)'
+      curraccordionArrow.style.transition = '0.2s'
+
     } else {
-      currAccContent.style.display = 'none'
+      currAccHeader.style.fontWeight = ''
+      currAccContent.style.maxHeight = null
+      currAccContent.style.marginBottom = '0px'
+      curraccordionArrow.style.transform = 'rotate(0)'
+      curraccordionArrow.style.transition = '0.2s'
+      
     }
   })
   
